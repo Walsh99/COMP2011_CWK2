cc50b751f353922f08f558f86aa3852010ff78e4
Walsh99 <146854530+Walsh99@users.noreply.github.com>
2024-12-04 19:11:36 +0000

    Delete app directory

--- app/static/js/account.js
+++
@@ -1,9 +0,0 @@
-// simple javascript to toggle the new password forms if option checked
-function togglePasswordFields() 
-{
-    const passwordFields = document.querySelectorAll('.password-fields');
-    for (let i = 0; i < passwordFields.length; i++) 
-    {
-        passwordFields[i].classList.toggle('d-none');
-    }
-}
